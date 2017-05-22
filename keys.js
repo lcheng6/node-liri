b8a2fd2bae0ec498aff2050e56e283ed4531063e
Liang Cheng <liangcheng6@gmail.com>
2017-05-22 01:14:44 -0400

    first commit

---
+++ keys.js
@@ -0,0 +1,12 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'MgjyFXbfjdLQqEpZVFRP2am31',
+  consumer_secret: 'rdlAcITe0QxcxeQDBzwQRF8gHuurMXAt1eVY7PpYVgmN9rSCEJ',
+  access_token_key: '56035358-iCnrEWcYCX6ypfPS1iaKO3mnLaBi9Jx1FNeIyV3Ah',
+  access_token_secret: 'uQAovncetJYd2XvJnbT83jcU9XbRa4BkEdFTc8aHQwjXJ',
+}
+
+exports.spotifyKeys= {
+
+}
---
+++ liri.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var twitterAPI = require('twitter');
+var request = require('request');
+var imdbAPI = require('imdb');
+var keys = require('./keys.js');
+var fs = require('fs');
+
+//console.log('hi this is Liang')
+
+
+var command = process.argv[2];
+var name = process.argv[3];
+
+var commands = {
+	"my-tweets": getTweets, 
+	"spotify-this-song": null,
+	"movie-this": null, 
+	"do-what-it-says": null
+}
+
+var twitterClient = new twitterAPI({
+    consumer_key: keys.twitterKeys.consumer_key,
+    consumer_secret: keys.twitterKeys.consumer_secret,
+    access_token_key: keys.twitterKeys.access_token_key,
+    access_token_secret: keys.twitterKeys.access_token_secret
+});
+
+
+function getTweets() {
+    twitterClient.get('statuses/user_timeline', { count: 20 }, function(error, tweets, response) {
+        if (!error) {
+            tweets.forEach(function(tweet) {
+                console.log('*********************************************************** \n Created: ' + tweet.created_at + "\n" + "Tweet: " + tweet.text);
+                console.log('Created: ' + tweet.created_at + "\n Tweet: " + tweet.text + "\n\n");
+            });
+        } else {
+            console.log(error);
+        }
+    })
+};
+
