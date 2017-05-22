af6e928f95aaee9e33ca63338d20519417ef52bd
Liang Cheng <liangcheng6@gmail.com>
2017-05-22 01:43:55 -0400

    movie this is done.  Need OMDB api key

--- liri.js
+++ liri.js
@@ -16,7 +16,7 @@ var name = process.argv[3];
 var commands = {
 	"my-tweets": getTweets, 
 	"spotify-this-song": spotify_this_song,
-	"movie-this": null, 
+	"movie-this": movie_this, 
 	"do-what-it-says": null
 }
 
@@ -28,6 +28,7 @@ var twitterClient = new twitterAPI({
 });
 
 
+//Get Tweets to get the last 20 tweets. 
 function getTweets() {
     twitterClient.get('statuses/user_timeline', { count: 20 }, function(error, tweets, response) {
         if (!error) {
@@ -41,9 +42,10 @@ function getTweets() {
     })
 };
 
+//Spotify this song function to do the spotify serach function.  
 function spotify_this_song(name) {
-	if(name ===null) {
-		name = "'The Sign Ace of Base'"
+	if(name === undefined) {
+		name = "The Sign Ace of Base"
 	}
 
 	spotifyAPI.search({ type: "track", query: name }, function(err, data) {
@@ -70,6 +72,37 @@ function spotify_this_song(name) {
     })
 }
 
+//got to OMDB API to get the title, year, actors, etc, etc, of a movie
+function movie_this(name) {
+	if (name === undefined) {
+		name = "James Bond"
+	}
+
+	console.log("name: " + name);
+
+    request.get("http://www.omdbapi.com/?apikey=<>t=" + name + "&plot=full", function(error, status, body) {
+        if (error) {
+        	console.log(error);
+        }else {
+        	var bodyObj = JSON.parse(body);
+	        // var ratings = "";
+	        // data.Ratings.forEach(function(rating) {
+	        //     ratings += rating.Source + ", ";
+	        // });
+	        // console.log("Ratings: " + ratings);
+	        console.log(body)
+	        console.log("Title: " + bodyObj.Title);
+	        console.log("Year: " + bodyObj.Year);
+	        console.log("Rating: " + bodyObj.imdbRating);
+	        console.log("Country: " + bodyObj.Country);
+	        console.log("Language: " + bodyObj.Language);
+	        console.log("Plot: " + bodyObj.Plot);
+	        console.log("Actors: " + bodyObj.Actors);
+        }
+    });
+}
+
+//the main function of the program.
 if (commands[command] != undefined) {
 	var func = commands[command];
 	if (func != null) {
